d7bbdbd3b0042a883d3fae0e47e0507fd5558792
Bernardo Amaral <bernardo.amaral@hogarthww.com>
2017-10-02 16:11:50 -0300

    migrate

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -15,7 +15,7 @@ class DefaultController extends Controller
             'rfid' => $rfid,
             'access' => false,
         ];
-        $user = \App\User::find(['rfidkey' => $rfid, 'active' => true])->first();
+        $user = \App\User::where(['rfidkey' => $rfid, 'active' => true])->first();
         if ($user) {
             $reply = [
                 'rfid' => $rfid,
