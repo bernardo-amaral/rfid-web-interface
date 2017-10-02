fcc0660b9f86cd953e37a00c2dfbca8f7aa02b80
Bernardo Amaral <bernardo.amaral@hogarthww.com>
2017-10-02 15:09:44 -0300

    migrate

--- app/Http/Controllers/PanelController.php
+++ app/Http/Controllers/PanelController.php
@@ -12,16 +12,30 @@ class PanelController extends Controller
     public function index(Request $request)
     {
         $data = $request->all();
-
         $users = \App\User::all();
-
         return view('panel.index', compact('users'));
     }
 
+    /**
+     * EnableUser
+     * @return response
+     */
     public function enableUser($id)
     {
         $user = \App\User::find($id);
         $user->active = true;
+        $user->save();
+        return redirect()->route('userList');
+    }
+
+    /**
+     * DisableUser
+     * @return response
+     */
+    public function disableUser($id)
+    {
+        $user = \App\User::find($id);
+        $user->active = false;
         $user->save();
         return redirect()->route('userList');
     }
