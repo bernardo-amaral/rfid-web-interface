513586d5157d4d571596f8e6cbc44883f94fdad7
Bernardo Amaral <bernardo.amaral@hogarthww.com>
2017-10-02 16:10:19 -0300

    migrate

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -6,17 +6,24 @@ use Illuminate\Http\Request;
 class DefaultController extends Controller
 {
     /**
-     * Index page
-     * @return view
+     * Check RFID BY ID
+     * @return JSON
      */
     public function checkRfid($rfid)
     {
-        $user = \App\User::find(['rfidkey' => $rfid, 'active' => true])->first();
-        dd($user->count());
-        return [
+        $reply = [
             'rfid' => $rfid,
-            'access' => true,
+            'access' => false,
         ];
+        $user = \App\User::find(['rfidkey' => $rfid, 'active' => true])->first();
+        if ($user) {
+            $reply = [
+                'rfid' => $rfid,
+                'username' => $user->name,
+                'access' => true,
+            ];
+        }
+        return $reply;
     }
 
 }
